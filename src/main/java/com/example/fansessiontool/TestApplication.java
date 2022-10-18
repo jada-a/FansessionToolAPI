6d6c0d7cc3cc908711098ec7943bad5010717285
Jada A <jada.alvarez29@gmail.com>
2022-10-18 13:26:53 -0400

    Added a test controller

--- src/main/java/com/example/fansessiontool/TestApplication.java
+++ src/main/java/com/example/fansessiontool/TestApplication.java
@@ -2,6 +2,8 @@ package com.example.fansessiontool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TestApplication {
@@ -10,4 +12,12 @@ public class TestApplication {
         SpringApplication.run(TestApplication.class, args);
     }
 
+}
+
+@RestController
+class HelloController{
+    @GetMapping("/")
+    String hello(){
+        return "Test";
+    }
 }
